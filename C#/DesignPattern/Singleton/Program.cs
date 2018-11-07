0de31482dd8d8abf4c9b6c94c45b9758934a2ed1
Rama Krishna Jinka <jinkaramakrishna@hotmail.com>
2018-11-07 18:52:37 -0500

    Add files via upload

---
+++ C#/DesignPattern/Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton Instance = new Singleton();
+        private int numberofinstance = 0;
+
+        private Singleton()
+        {
+            Console.WriteLine("Instantiating inside the private constructor.");
+            numberofinstance++;
+            Console.WriteLine("Number of Instances = {0}", numberofinstance);
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                Console.WriteLine("We already have an Instance, use it");
+                return Instance;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tring to create first instance");
+            Singleton FirstInstance = Singleton.GetInstance;
+            Console.WriteLine("Tring to create second instance");
+            Singleton SecondInstance = Singleton.GetInstance;
+            if(FirstInstance == SecondInstance)
+            {
+                Console.WriteLine("only one instance exists.");
+            }
+            else
+            {
+                Console.WriteLine("Different instance exists.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
